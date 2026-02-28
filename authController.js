0cb2327e434b09d2a5824912d9367a9c5e2d6444
MezGAM <al07039705@tecmilenio.mx>
2026-02-27 23:12:00 -0600

    commit 3

--- authController.js
+++ authController.js
@@ -1,16 +1,14 @@
-const db = require('../config/db'); // Tu conexión a MySQL
+const db = require('../config/db'); 
 const bcrypt = require('bcryptjs');
-const { enviarBienvenida } = require('../utils/mailer');
+const { enviarBienvenida } = require('mailer');
 
 exports.registrar = async (req, res) => {
     try {
         const { username, email, password, mascota } = req.body;
 
-        // 1. Encriptar contraseña con Bcrypt
         const salt = await bcrypt.genSalt(10);
         const passwordHash = await bcrypt.hash(password, salt);
 
-        // 2. Insertar en MySQL
         const sql = 'INSERT INTO usuarios (username, email, password, mascota_elegida) VALUES (?, ?, ?, ?)';
         db.query(sql, [username, email, passwordHash, mascota], async (err, result) => {
             if (err) {
@@ -18,7 +16,6 @@ exports.registrar = async (req, res) => {
                 return res.status(500).json({ error: "Error en la base de datos" });
             }
 
-            // 3. Enviar correo de bienvenida
             try {
                 await enviarBienvenida(email, username);
             } catch (mailErr) {
