5c3cf18fd109c7bd0b1395ac1e1d36ea22d82c3c
MezGAM <al07039705@tecmilenio.mx>
2026-02-27 23:30:39 -0600

    commit 4

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mysql = require('mysql2/promise');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
-const { enviarBienvenida } = require('mailer');
+const { enviarBienvenida } = require('./mailer');
 
 const app = express();
 app.use(express.json());
